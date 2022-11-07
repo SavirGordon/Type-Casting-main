32101ed2be5b42549314034a58371ab959d21eda
SavirGordon <savirgordon83@gmail.com>
2022-11-07 11:57:53 -0500

    k

--- Lab - Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
+++ Lab - Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
@@ -43,10 +43,27 @@ var workouts: [Workout] = [
     Run(cadence: 90, time: 358.9, distance: 1600)
 ]
 //:  Write simple functions called `describeRun(runningWorkout:)` and `describeSwim(swimmingWorkout:)` that take a `Run` object and a `Swim` object, respectively. Neither should return values. Each function should print a description of the workout, including the run's cadence or the swim's stroke. Time is represented in seconds, distance is represented in meters, and cadence is represented in steps per minute.
- 
+func describeRun(runningWorkout: Run) {
+    print(runningWorkout.distance)
+    print(runningWorkout.time)
+    print(runningWorkout.cadence)
+}
+
+func describeSwim(swimmingWorkout: Swim) {
+    print(swimmingWorkout.distance)
+    print(swimmingWorkout.time)
+    print(swimmingWorkout.stroke)
+}
 
 //:  Now loop through each workout in `workouts` and, using type casting, call either `describeRun(runningWorkout:)` or `describeSwim(swimmingWorkout:)` on each. Observe what is printed to the console.
- 
+for workout in workouts {
+    if let workout = workout as? Run {
+        describeRun(runningWorkout: workout)
+    }
+    else if let workout = workout as? Swim {
+        describeSwim(swimmingWorkout: workout)
+    }
+}
 
 /*:
  _Copyright © 2021 Apple Inc._
